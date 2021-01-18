232a37fc1a9ef5b37505b029f8f38b65bb7907a5
NikolaiSurodin <kolya_12.93@mail.ru>
2021-01-18 18:35:14 +0700

    chart

--- src/components/company/companyDetail.vue
+++ src/components/company/companyDetail.vue
@@ -22,16 +22,23 @@
         </ul>
       </div>
       <div class="card-content grey lighten-4">
-        <div id="test4">Название компании: {{ companies.name }}</div>
-        <div id="test5">Адрес: {{ companies.address }}</div>
-        <div id="test6">ID: {{ companies.id }}</div>
+        <div class="chart">
+          <p style="text-align: center">Диаграмма соотношение численности по отделам</p>
+          <canvas ref="canvas"/>
+        </div>
+        <div class="info">
+          <p>Название компании: {{ companies.name }}</p>
+          <p>Адрес: {{ companies.address }}</p>
+          <p>ID: {{ companies.id }}</p>
+        </div>
+
       </div>
       <div>
         <button class="btn-small" @click="toCompanies">Назад</button>
       </div>
     </div>
     <!--  чтобы не ругалось в методе, пока роут не определен -->
-    <div v-if="employees">
+    <div class="col" v-if="employees">
       <!-- МЕТОД!!! function() -->
       <ul v-for="(emp, key) in employeeOnDepartments()"
           :key="key">
@@ -40,15 +47,16 @@
         </li>
       </ul>
     </div>
-
   </div>
 </template>
 
 <script>
 import Department from '@/servces/Department'
+import {Pie} from 'vue-chartjs'
 
 export default {
   name: 'companyDetail',
+  extends: Pie,
   data() {
     return {
       department: Department
@@ -66,6 +74,13 @@ export default {
     toCompanies() {
       this.$router.push('/company')
     },
+    empChart() {
+      if (this.$route.hash !== '') {
+        return  this.employees.filter(el => el.department === this.department[this.$route.hash.replace('#', '')])
+      }
+      console.log(this.employees)
+      return this.employees
+    }
   },
   computed: {
     companies() {
@@ -75,10 +90,55 @@ export default {
     employees() {
       return this.$store.getters.employees.filter(el => el.company_id === this.$route.params['id'])
     },
+  },
+  mounted() {
+    this.empChart()
+    this.renderChart({
+      labels: Object.values(this.department),
+      datasets: [{
+        label: 'Количесво сотрудников отдела',
+        data: this.empChart(),
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255, 99, 132, 1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      }]
+    })
+
+
+
   }
 }
 </script>
 
 <style scoped>
+.chart {
+  height: 500px;
+  width: 500px;
+  position: center;
+  margin-left: 500px;
+  margin-top: auto;
+}
 
+.col {
+  max-width: 550px;
+}
+
+.info {
+  margin-top: auto;
+  display: block;
+}
 </style>
